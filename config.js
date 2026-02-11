321066f27788ecfe74cc117102aaac53fdf307fa
misbahmehdi04 <misbahmehdi14@gmail.com>
2026-02-11 23:00:13 +0500

    Resume Buider

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // config.js
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-const SUPABASE_URL = 'https://jjdeklsuwzasxvfsabmg.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpqZGVrbHN1d3phc3h2ZnNhYm1nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk4NDcxMjUsImV4cCI6MjA4NTQyMzEyNX0.U0VCCur9yJn6WFS0M_bsSfBSrGiEFUFbc20NTLSXqog';
+const SUPABASE_URL = 'https://zyqxgfayxgimzcfhmgfu.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_xWBy_O9d0D9S7_mBoIO4Zw_uWBtPVsi';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
